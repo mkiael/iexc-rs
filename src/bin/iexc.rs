f9d1d2b1ff9a51031a621fc11ea7b5f39930c32a
Mikael Larsson <c.mikael.larsson@gmail.com>
2021-05-12 23:35:55 +0200

    Add stub for iexc client

--- src/bin/iexc.rs
+++ src/bin/iexc.rs
@@ -1,7 +1,5 @@
-use iexc::http::{Client};
+use iexc;
 
 fn main() {
-    let client = Client::new("httpbin.org".to_string());
-    let response = client.get("/get").unwrap();
-    println!("{}", response.body.trim_end());
+    let _client = iexc::Client::new("mytoken".to_string());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,22 @@
-pub mod http;
+mod http;
+
+pub struct Client {
+    http_client: http::Client,
+    api_token: String,
+}
+
+impl Client {
+    pub fn new(api_token: String) -> Self {
+        Self {
+            http_client: http::Client::new("cloud.iexapis.com".to_string()),
+            api_token,
+        }
+    }
+
+    pub fn new_sandbox(api_token: String) -> Self {
+        Self {
+            http_client: http::Client::new("https://sandbox.iexapis.com".to_string()),
+            api_token,
+        }
+    }
+}
