eb7ec7577d87f8917ff00f339414c9334a6d15dc
Mikael Larsson <c.mikael.larsson@gmail.com>
2021-05-13 23:41:10 +0200

    Handle api token from command line argument

--- src/bin/iexc.rs
+++ src/bin/iexc.rs
@@ -1,5 +1,23 @@
 use iexc;
+use clap::{App, Arg};
 
 fn main() {
-    let _client = iexc::Client::new("mytoken".to_string());
+    let matches = App::new("IEXC")
+        .version("0.1")
+        .author("Mikael L. <c.mikael.larsson@gmail.com>")
+        .arg(
+            Arg::new("API token")
+            .short('a')
+            .long("api-token")
+            .value_name("TOKEN")
+            .about("API token used for all requests to IEX endpoints")
+            .takes_value(true),
+        )
+        .get_matches();
+
+    if let Some(api_token) = matches.value_of("API token") {
+        let _client = iexc::Client::new(api_token.to_string());
+    } else {
+        println!("No API token given");
+    }
 }
